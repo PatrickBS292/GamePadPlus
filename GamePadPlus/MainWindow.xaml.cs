414a1db78c83f6d4a49760f63e8d6fa072e70690
PatrickBS292 <patrickblicher@gmail.com>
2026-09-02 12:11:41 +0200

    Commit

--- GamePadPlus/MainWindow.xaml.cs
+++ GamePadPlus/MainWindow.xaml.cs
@@ -2,13 +2,24 @@
 
 namespace GamePadPlus
 {
-
-    public partial class  MainWindow: Window
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
             MainFrame.Navigate(new LibraryPage());
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new LibraryPage());
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new InfoPage());
         }
     }
 }
+
